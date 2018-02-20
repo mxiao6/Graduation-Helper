73bd7fd225f95e54d7955c7dfd8106c422d55241
qpdang2 <qpdang2@illinois.edu>
2018-02-20 03:52:35 -0600

    Added some course api data pulling

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const api = require('./api.js');
+
+app.use('/api', api);
 
 app.get('/hello', function(req, res){
   res.send("Hello World");
-});
-
-app.get('/api/hello', function(req, res){
-  res.send({express: 'Hello from express'});
 });
 
 app.get('*', function(req, res){
