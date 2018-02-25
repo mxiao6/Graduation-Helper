20d42ef99bc98b314b4650227bf7f6aa780ecb32
Mingze Xiao <mxiao666@gmail.com>
2018-02-25 16:41:16 -0600

    add back public for deploy on azure

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 var path = require('path');
 
-// app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/hello', function(req, res){
