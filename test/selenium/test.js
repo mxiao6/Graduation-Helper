eec5c046087a37946de6b2d166e4ab063e91507d
Sergey Papushin <spapushin@gmail.com>
2018-02-26 16:29:27 -0600

    Use no sandbox mode for Chrome

--- test/selenium/test.js
+++ test/selenium/test.js
@@ -7,12 +7,12 @@ var should = chai.should();
 
 describe( 'Test Suite' , function(){
 
-    var driver = new webdriver.Builder().forBrowser('chrome').build();
-
-    before(function(){
-
-        driver.get( 'http://localhost:3000/' );
-    });
+    var chromeCapabilities = webdriver.Capabilities.chrome();
+    var chromeOptions = {
+        'args': ['--no-sandbox']
+    };
+    chromeCapabilities.set('chromeOptions', chromeOptions);
+    var driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
 
     after(function(){
         driver.quit();
@@ -20,11 +20,11 @@ describe( 'Test Suite' , function(){
 
     it( 'App Title' , function(done){
         this.timeout(10000)
-        driver.get( 'http://www.google.com/' )
+        driver.get( 'http://localhost:3000/' )
             .then(() => driver.getTitle())
 
             .then(title =>
-            title.should.equal('Google')
+            title.should.equal('React App')
         )
             .then(() => done())
             .catch(error => done(error))
