a1cbcdd916625aaba0242cc559e4193d76eb393e
Sergey Papushin <spapushin@gmail.com>
2018-03-01 16:00:35 -0600

    Revert server.js changes

--- server.js
+++ server.js
@@ -10,15 +10,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/api', api);
-
-app.post('/register',userlogin.register);
-
-app.post('/login',userlogin.login);
-
+app.post('/register', userlogin.register);
+app.post('/login', userlogin.login);
 
 process.on('uncaughtException', (err) => {
-  console.log("uncaughtException " + err);
-
+  console.log('uncaughtException ' + err);
 });
 
 // for front end
