f32b9782420be554ea469f346211ad918bdb5ba9
Sergey Papushin <spapushin@gmail.com>
2018-03-01 15:25:40 -0600

    Check for standard style in continuous integration

--- routes/userlogin.js
+++ routes/userlogin.js
@@ -1,84 +1,79 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
 
-//var nodemailer = require('nodemailer');
-//var randomstring = require('randomstring');
+// var nodemailer = require('nodemailer');
+// var randomstring = require('randomstring');
 
 var connection = mysql.createConnection({
-  //information for connecting to Azure database
-  
-  host:'graduationhelper.mysql.database.azure.com',
-  user:'myadmin@graduationhelper',
-  password:'Cs428grh!',
-  database:'graduation_helper'
-  
-  //Please try to use local database to test!!!
+  // information for connecting to Azure database
+
+  host: 'graduationhelper.mysql.database.azure.com',
+  user: 'myadmin@graduationhelper',
+  password: 'Cs428grh!',
+  database: 'graduation_helper'
+
+  // Please try to use local database to test!!!
   // host:'localhost',
   // user:'xxx',
   // password:'xxx',
   // database:'xxx'
-});
+})
 
+connection.connect(function (err) {
+  if (!err) {
+    console.log('sucessfully connected to database')
+  } else {
+    console.log('Database error')
+  }
+})
 
-connection.connect(function(err){
-if(!err) {
-    console.log("sucessfully connected to database");
-} else {
-    console.log("Database error");
-}
-});
-
-exports.register = function(req,res){
-  var users={
-    "username":req.body.username,
-    "email":req.body.email,
-    "password":req.body.password,
+exports.register = function (req, res) {
+  var users = {
+    'username': req.body.username,
+    'email': req.body.email,
+    'password': req.body.password
   }
   connection.query('SELECT * FROM users WHERE email = ?', [req.body.email], function (error, results, fields) {
-  //check for duplicate register
-  if (error) {
-    res.send("error ocurred");
-  }else{
-    if(results.length >0){
-      res.send("Email already registered!")
-    }
-    else{
-        connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-        if (error) {
-        //console.log("error ocurred",error);
-        res.send("error ocurred");
-        }else{
-        //console.log('The solution is: ', results);
-        res.send("user registered sucessfully");
-        }
-      });
-    }
-  }
-  });
-
-}
-
-exports.login = function(req,res){
-  var email = req.body.email;
-  var password = req.body.password;
-  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+  // check for duplicate register
     if (error) {
-      res.send("error ocurred");
-    }else{
-      if(results.length > 0){
-        if(results[0].password === password){
-          res.send("login sucessfull");
-        }
-        else{
-          res.send("Email and password does not match");
-        }
-      }
-      else{
-        res.send("Email does not exist");
+      res.send('error ocurred')
+    } else {
+      if (results.length > 0) {
+        res.send('Email already registered!')
+      } else {
+        connection.query('INSERT INTO users SET ?', users, function (error, results, fields) {
+          if (error) {
+            // console.log("error ocurred",error);
+            res.send('error ocurred')
+          } else {
+            // console.log('The solution is: ', results);
+            res.send('user registered sucessfully')
+          }
+        })
       }
     }
-  });
+  })
 }
-//for password reset:
+
+exports.login = function (req, res) {
+  var email = req.body.email
+  var password = req.body.password
+  connection.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+    if (error) {
+      res.send('error ocurred')
+    } else {
+      if (results.length > 0) {
+        if (results[0].password === password) {
+          res.send('login sucessfull')
+        } else {
+          res.send('Email and password does not match')
+        }
+      } else {
+        res.send('Email does not exist')
+      }
+    }
+  })
+}
+// for password reset:
 
 /*
 Need test
@@ -124,15 +119,15 @@ exports.sendemail = function(req,res){
   });
 }
 */
-exports.resetpassword = function(req,res){
-  //called when user is authorized to reset password
-  var email = req.body.email;
-  var password = req.body.password;
-  connection.query('UPDATE users SET password = ? WHERE email = ?',[password, email],function (error,results, fields) {
+exports.resetpassword = function (req, res) {
+  // called when user is authorized to reset password
+  var email = req.body.email
+  var password = req.body.password
+  connection.query('UPDATE users SET password = ? WHERE email = ?', [password, email], function (error, results, fields) {
     if (error) {
-      res.send("error ocurred");
-    }else{
-      res.send("Reset successfully!");
+      res.send('error ocurred')
+    } else {
+      res.send('Reset successfully!')
     }
-  });
+  })
 }
