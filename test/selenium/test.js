2ece6333a04c2e70bcc239bcb3031f2705fa1e7e
Sergey Papushin <spapushin@gmail.com>
2018-03-01 17:17:21 -0600

    Add login button test

--- test/selenium/test.js
+++ test/selenium/test.js
@@ -1,6 +1,4 @@
 var webdriver = require('selenium-webdriver');
-var By = webdriver.By;
-var until = webdriver.until;
 require('chromedriver');
 require('chai').should();
 
@@ -16,27 +14,31 @@ describe('Test Suite', function () {
     driver.quit();
   });
 
-  it('App Title', function (done) {
-    this.timeout(5000);
+  function findLoginButton () {
+    return driver.findElements(webdriver.By.css('.loginButton a')).then(function (result) {
+      return result[0];
+    });
+  }
+
+  it('Page Title', function (done) {
+    this.timeout(10000);
     driver.get('http://localhost:3000/')
       .then(() => driver.getTitle())
       .then(title =>
-        title.should.equal('React App')
+        title.should.equal('Graduation Helper')
       )
       .then(() => done())
       .catch(error => done(error));
+  });
 
-    var elem = driver.findElement(By.css('a=[href=#/Login]'));
-    elem.click();
-
-    driver.wait(until.urlContains('Login'), 20000);
-
-    driver.wait(until.elementLocated(By.className('ant-form-item-control')), 2000);
-    driver.wait(until.elementLocated(By.css('input[type=text][id=email]')));
-    var email = driver.findElement(By.css('input[type=text][id=email]'));
-    email.sendKeys('zuyichen@hotmail.com');
-    driver.wait(until.elementLocated(By.css('input[type=password]')));
-    var password = driver.findElement(By.css('input[type=password'));
-    password.sendKeys('pass');
+  it('Login Button on Home Page', function (done) {
+    this.timeout(10000);
+    driver.get('http://localhost:3000/')
+      .then(() => driver.wait(findLoginButton, 2000))
+      .then(button => button.click())
+      .then(() => driver.getCurrentUrl())
+      .then(curUrl => curUrl.should.include('Login'))
+      .then(() => done())
+      .catch(error => done(error));
   });
 });
