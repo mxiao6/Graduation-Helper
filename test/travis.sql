2f648a680710f1fa28701dfa9e5119d32c6a6bcf
Zuyi Chen <zchen138@illinois.edu>
2018-03-13 14:17:13 -0500

    new sql file

--- test/travis.sql
+++ test/travis.sql
@@ -1,25 +1,21 @@
 CREATE DATABASE testDatabase;
 
-CREATE TABLE testDatabase.users {
-  user_id int(11) AUTO_INCREMENT,
-  username varchar(16),
-  email varchar(32),
-  password varchar(32),
-  PRIMARY_KEY (user_id)
-}
+CREATE TABLE testDatabase.users (
+  user_id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  username varchar(16) NOT NULL,
+  email varchar(32) NOT NULL,
+  password varchar(32) NOT NULL
+)
+CREATE TABLE testDatabase.courses(
+  course_id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  subject varchar(4) NOT NULL,
+  course_number int(11) NOT NULL,
+  cru int(11) NOT NULL,
+  schedule_id int(11) NOT NULL
+)
 
-CREATE TABLE testDatabase.courses{
-  course_id int(11) AUTO_INCREMENT,
-  subject varchar(4),
-  course_number int(11),
-  cru int(11),
-  schedule_id int(11),
-  PRIMARY_KEY (course_id)
-}
-
-CREATE TABLE testDatabase.schedules{
-  schedule_id int(11) AUTO_INCREMENT,
-  semester varchar(8),
-  user_id int(11),
-  PRIMARY_KEY (schedule_id)
-}
+CREATE TABLE testDatabase.schedules(
+  schedule_id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  semester varchar(8) NOT NULL,
+  user_id int(11) NOT NULL
+)
