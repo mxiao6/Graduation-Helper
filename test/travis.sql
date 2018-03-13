669cdf156bea07d83168f905d668a1b8ca1b7444
Zuyi Chen <zchen138@illinois.edu>
2018-03-13 14:31:28 -0500

    sql file fix

--- test/travis.sql
+++ test/travis.sql
@@ -5,14 +5,15 @@ CREATE TABLE testDatabase.users (
   username varchar(16) NOT NULL,
   email varchar(32) NOT NULL,
   password varchar(32) NOT NULL
-)
+);
+
 CREATE TABLE testDatabase.courses(
   course_id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
   subject varchar(4) NOT NULL,
   course_number int(11) NOT NULL,
   cru int(11) NOT NULL,
   schedule_id int(11) NOT NULL
-)
+);
 
 CREATE TABLE testDatabase.schedules(
   schedule_id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
