1ebc2811df5aa03c1c33852e3ec49c09f4389f47
Sergey Papushin <spapushin@gmail.com>
2018-03-16 15:14:19 -0500

    Add Semester Selection test

--- test/selenium/test.js
+++ test/selenium/test.js
@@ -224,5 +224,70 @@ describe('Home and Login Tests', function () {
   });
 });
 
+function findSelectClassButton () {
+  return driver.findElements(webdriver.By.linkText('Select Class')).then(function (result) {
+    return result[0];
+  });
+}
+
+function findSemesterDropdown () {
+  return driver.findElements(webdriver.By.css('.ant-cascader-input')).then(function (result) {
+    return result[0];
+  });
+}
+
+function findElementByTitle (title) {
+  return driver.findElements(webdriver.By.css('*[title="' + title + '"]')).then(function (result) {
+    return result[0];
+  });
+}
+
+function findNextButton () {
+  return driver.findElements(webdriver.By.linkText('Next')).then(function (result) {
+    return result[0];
+  });
+}
+
+function findSelectedSemester () {
+  return driver.findElements(webdriver.By.css('.contentContainer div')).then(function (result) {
+    return result[0];
+  });
+}
+
 describe('Schedule Generation Tests', function () {
+  it('Select Semester', function (done) {
+    this.timeout(20000);
+    driver.get('http://localhost:3000/')
+      .then(() => driver.wait(findLoginPageButton, 2000))
+      .then(button => button.click())
+      .then(() => driver.wait(findEmailEntry(), 2000))
+      .then(input => input.sendKeys('admin@gmail.com'))
+      .then(() => driver.wait(findPasswordEntry(), 2000))
+      .then(input => input.sendKeys('test'))
+      .then(() => driver.wait(findLoginButton, 2000))
+      .then(button => button.click())
+      .then(() => driver.sleep(2000))
+      .then(() => driver.wait(findSelectClassButton, 2000))
+      .then(button => button.click())
+      .then(() => driver.getCurrentUrl())
+      .then(url => url.should.contain('SemesterSelection'))
+      .then(() => driver.sleep(2000))
+      .then(() => driver.wait(findSemesterDropdown(), 2000))
+      .then(dropdown => dropdown.click())
+      .then(() => driver.wait(findElementByTitle('2018'), 2000))
+      .then(element => element.click())
+      .then(() => driver.sleep(2000))
+      .then(() => driver.wait(findElementByTitle('Fall 2018'), 2000))
+      .then(element => element.click())
+      .then(() => driver.wait(findNextButton(), 2000))
+      .then(button => button.click())
+      .then(() => driver.sleep(2000))
+      .then(() => driver.getCurrentUrl())
+      .then(url => url.should.contain('ClassSelection'))
+      .then(() => driver.wait(findSelectedSemester(), 2000))
+      .then(textElem => textElem.getAttribute('innerText'))
+      .then(titleText => titleText.should.contain('Selected Semester: Fall 2018'))
+      .then(() => done())
+      .catch(error => done(error));
+  });
 });
