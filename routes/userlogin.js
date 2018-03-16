0fe8896c3dcac38df6b4c3ed7cbe32222edd0200
George-Wu <jwu108@illinois.edu>
2018-03-15 21:37:59 -0500

    change status code

--- routes/userlogin.js
+++ routes/userlogin.js
@@ -76,7 +76,7 @@ exports.login = function (req, res) {
       } else {
         if (results.length > 0) {
           if (results[0].password === password) {
-            res.send('login sucessfull');
+            res.status(200).send('login sucessfull');
           } else {
             res.status(422).send('Email and password does not match');
           }
