0290c7c455b2daf7a7be57ed1fb912c933d08bcf
Mingze Xiao <mxiao666@gmail.com>
2018-03-27 13:27:14 -0500

    add redux logger; minor style changes

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,16 +1,14 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
-import rootReducer from "./reducer";
+import rootReducer from './reducer';
 
 // let middleware = [thunkMiddleware, promiseMiddleware];
-let middleware = [thunkMiddleware];
+let middleware = [thunkMiddleware, createLogger()];
 
 function configureStore() {
-  const store = createStore(
-    rootReducer,
-    applyMiddleware(...middleware)
-  );
+  const store = createStore(rootReducer, applyMiddleware(...middleware));
 
   return store;
 }
