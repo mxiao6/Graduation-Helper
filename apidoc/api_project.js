1303b674eac87878a311963156405ec4b8d3c387
George-Wu <jwu108@illinois.edu>
2018-04-20 00:02:07 -0500

    update apidoc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,27 +1,1 @@
-define({
-  "name": "Graduation Helper",
-  "version": "0.3.0",
-  "description": "apiDoc for backend",
-  "title": "API documentation GRH",
-  "order": [
-    "GetYears",
-    "GetSemester",
-    "GetSubjects",
-    "GetCourses",
-    "GetSections",
-    "GetSectionDetails"
-  ],
-  "template": {
-    "withCompare": true,
-    "withGenerator": true
-  },
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2018-04-19T23:41:38.669Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "name": "Graduation Helper",  "version": "0.3.0",  "description": "apiDoc for backend",  "title": "API documentation GRH",  "order": [    "GetYears",    "GetSemester",    "GetSubjects",    "GetCourses",    "GetSections",    "GetSectionDetails"  ],  "template": {    "withCompare": true,    "withGenerator": true  },  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-04-20T05:01:46.793Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
